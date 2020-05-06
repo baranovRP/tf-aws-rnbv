34d375f3730328b4491daa344bb44a6e0d7790a3
baranovrp <baranov.r.p@gmail.com>
2020-05-06 20:54:16 +0200

    Add lock on state of webserver-cluster

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -6,8 +6,21 @@ provider "aws" {
   profile                 = "ora2postgres"
 }
 
+terraform {
+  required_version = "~> v0.12"
+
+  backend "s3" {
+    bucket = "tf-state-eu-west-2-rnbv"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "eu-west-2"
+
+    dynamodb_table = "tf-locks-eu-west-2-rnbv"
+    encrypt        = true
+  }
+}
+
 module "webserver_cluster" {
-  source = "github.com/baranovRP/tf-aws-rnbv-modules//modules/services/webserver-cluster?ref=v0.0.1"
+  source = "github.com/baranovRP/tf-aws-rnbv-modules//modules/services/webserver-cluster?ref=v0.0.4"
 
   cluster_name           = "tf-webservers-stage"
   db_remote_state_bucket = "tf-state-eu-west-2-rnbv"
